aff2d0002e9866992859dbb12d89b9b68408f661
AJ <adam_289@hotmail.com>
2021-04-09 22:05:59 +0800

    Create data directory if it does not exist to contain log and database files

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+from pathlib import Path
 from tournamenttracker import create_app
 from setup_db import check_db_created
+
+# Create directories  if they do not exist
+# https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
+Path("./tournamenttracker/data/db").mkdir(parents=True, exist_ok=True)
+Path("./tournamenttracker/data/logs").mkdir(parents=True, exist_ok=True)
 
 app = create_app()
 
